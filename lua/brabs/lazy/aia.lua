4b7e996534ba32a7466a5822fc49c81b0d04692c
Branden Call <brandencall@live.com>
2025-06-29 11:03:37 -0600

    harpoon is the main update

--- lua/brabs/lazy/aia.lua
+++ lua/brabs/lazy/aia.lua
@@ -1,8 +1,11 @@
-
 return {
     {
         dir = "~/Projects/plugins/aia.nvim",
         name = "aia.nvim",
-        lazy = false
+        lazy = false,
+        config = function()
+            require("aia")
+            vim.notify("aia.nvim config executed", vim.log.levels.INFO)
+        end
     }
 }
