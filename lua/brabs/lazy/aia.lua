92e59a08c9f7cc619573116abff6f0b4bb275fa7
Branden Call <brandencall@live.com>
2025-07-21 15:49:13 -0600

    Added project window shortcut

--- lua/brabs/lazy/aia.lua
+++ lua/brabs/lazy/aia.lua
@@ -5,9 +5,14 @@ return {
         lazy = false,
         config = function()
             require("aia")
+
+            vim.o.statusline = "%f %h%m%r %{v:lua.require'aia.state'.get_status_icon()}%=%-14.(%l,%c%V%) %P"
             vim.keymap.set("n", "<leader>ba", function()
                 vim.cmd("AiFloatingWin")
             end, { desc = "Open floating window" })
+            vim.keymap.set("n", "<leader>bc", function()
+                vim.cmd("AiProjectWin")
+            end, { desc = "Open project window" })
         end
     }
 }
