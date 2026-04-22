279534a17488507edfe9c805e990bf968084eb07
Branden Call <brandencall@live.com>
2026-04-22 08:17:12 -0600

    Added some autocommands for lsp

--- lua/brabs/autocommands.lua
+++ lua/brabs/autocommands.lua
@@ -5,3 +5,19 @@ vim.api.nvim_create_autocmd("TextYankPost", {
         vim.highlight.on_yank()
     end,
 })
+
+vim.api.nvim_create_autocmd('LspAttach', {
+    callback = function(e)
+        local opts = { buffer = e.buf }
+        vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
+        vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+        vim.keymap.set("i", "<C-h>", vim.lsp.buf.signature_help, opts)
+        vim.keymap.set("n", "<leader>lws", vim.lsp.buf.workspace_symbol, opts)
+        vim.keymap.set("n", "<leader>lrr", vim.lsp.buf.references, opts)
+        vim.keymap.set("n", "<leader>ld", vim.diagnostic.open_float, opts)
+        vim.keymap.set("n", "<leader>lca", vim.lsp.buf.code_action, opts)
+        vim.keymap.set("n", "<leader>lrn", vim.lsp.buf.rename, opts)
+        vim.keymap.set("n", "[d", vim.diagnostic.goto_next, opts)
+        vim.keymap.set("n", "]d", vim.diagnostic.goto_prev, opts)
+    end
+})
